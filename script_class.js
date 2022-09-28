c96626388e12587f4d0400357efa24257042fae7
Andrej Lazic <48242465+AndrejLazic@users.noreply.github.com>
2022-09-28 16:41:02 +0200

    Update script_class.js
    odrajeno iscrtavanje postova
    merge dva JSON-a

--- script_class.js
+++ script_class.js
@@ -1,70 +1,47 @@
-let thePosts = [];
-let theUser = [];
-let theComments = [];
+async function getPost(){
+    const response =  await fetch('https://jsonplaceholder.typicode.com/posts');
+    var posts = await response.json();
+    //console.log(posts);
 
-async function jsonPost(){
-    const jsonPost = 'https://jsonplaceholder.typicode.com/posts';
-    const response =  await fetch(jsonPost);
-    const data = await response.json();
+    const response1 = await fetch('https://jsonplaceholder.typicode.com/users');
+    var users = await response1.json();
+    //console.log(users);
 
-    thePosts = data.map(post => new Post(post));
-
-    //console.log(thePosts);
-    //console.log(thePosts[9].getPostId());
-    for(let i=0; i<=thePosts.length; i++){
-        thePosts[i].createPost();
+    //merge two JSONs into one, based od id and postId
+    //to get username and post details in one JSON
+    var j=0;
+    var merge=[];
+    for(let i=0; i < posts.length; i++){
+        if(posts[i].userId == users[j].id){
+            merge.push({...users[j], ...posts[i]});
+        }else{
+        i--;
+        j++;
+        }
     }
     
+    const post = new Post(merge);
 }
-
-async function parseJsonUser(){
-    const jsonPost = 'https://jsonplaceholder.typicode.com/users';
-    const response =  await fetch(jsonPost);
-    const data = await response.json();
-
-    let theUser = data.map(user => new User(user));
-    //console.log(theUser);
-    //console.log(theUser[9].getUserId());
-}
-
-async function parseJsonComm(){
-    const jsonPost = 'https://jsonplaceholder.typicode.com/comments';
-    const response =  await fetch(jsonPost);
-    const data = await response.json();
-
-    let theComments = data.map(comm => new Comments(comm));
-
-    //console.log(theComments);
-    //console.log(theComments[9].getCommentId());
-}
-
-jsonPost();
-parseJsonUser();
-parseJsonComm();
 
 class Post{
     constructor(post){
-        //console.log("constructor Post")
-        this.title = post.title;
-        this.body = post.body;
-        this.id = post.id;
-        this.username = "ANDREJ";
+        this.article = post;
+        
+        console.log(this.article);
+
+        this.generatePosts();
     }
 
-    getPostId (){
-        return this.id;
-    }
-
-    createPost(){
-        let card = "";
-            
-                card +=  `
+    generatePosts(){
+        var card = "";
+        for(let i=0; i<this.article.length; i++){
+            card +=  `
                 <div class="container">             
                 <div class="col mb-4">
                     <div class="card bg-black p-2 " style="--bs-bg-opacity: .23;">
                         
                         <div class ="p-2 fs-5 text-left text-white">
-                            <div>${this.title}
+                            <div>${this.article[i].title}
                                 <div class="text-end">
                                     <div class="btn-group me-2" role="group">
                                         <button type="button" class="btn bg-black text-white" style="--bs-bg-opacity: .37;">+</button>
@@ -75,15 +52,15 @@ class Post{
                         </div>
                         
                         <div class ="card-body bg-black text-left text-white-50 rounded-3 m-2" style="--bs-bg-opacity: .37;">
-                            <div class="price fs-7" class="price fs-8">${this.body}</div>
+                            <div class="price fs-7" class="price fs-8">${this.article[i].body}</div>
                         </div>
                         
                         <div class="fs-6 text-white-50 mt-2 mb-2">
                             <div class="text-end">
                                 <abbr class="bg-black text-white-50 rounded-3 p-1" style="--bs-bg-opacity: .37;">
                                     <abbr title="attribute">pre 14 sati</abbr>
-                                    <span id="username" class="fst-italic">${this.username}</span>
-                                    <button id='${this.id}' onclick="getId(this)" class="btn text-white-50 fst-italic"><b>Comments</b></button>
+                                    <span id="username" class="fst-italic">${this.article[i].name}</span>
+                                    <button id='${this.article[i].id}' class="btn text-white-50 fst-italic"><b>Comments</b></button>
                                 </abbr>
                             </div>
                         </div>
@@ -92,67 +69,32 @@ class Post{
                 </div>
                 `;
             
+        }
         document.getElementById("cards").innerHTML = card;
     }
 
+    getArictleId(e){
+        let button = e.target;
+        let buttonId = getAttribute('id');
+        let buttonValue = button.value;
+
+        console.log(buttonId);
+        console.log(buttonValue);
+    }
 }
 
-class User{
-    constructor(user){
-        //console.log("constructor User")
-        this.name = user.name;
-        this.id = user.id;
-    }
+async function getComments(){
+    const response =  await fetch('https://jsonplaceholder.typicode.com/comments');
+    const comm = await response.json();
 
-    getUserId(){
-        return this.id;
-    }
-
+    const comments = new Comments(comm)
 }
 
 class Comments{
     constructor(comm){
-        //console.log("constructor Comments")
-        this.title =  comm.name;
-        this.body = comm.body;
-        this.email = comm.email;
-        this.postId = comm.postId;
+
     }
+    generateComments(){
 
-    getCommentId(){
-        return this.postId;
     }
-
-    createComments(){
-        var comment = 
-                    `
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                        
-                                <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">${this.email}</h5>
-                                </div>
-                            
-                                <div class="modal-body">
-                                ${this.title}
-                                </div>
-                
-                                <div class="modal-body">
-                                ${this.body}
-                                </div>
-            
-                            </div>
-                        </div>
-                    `;
-
-        document.getElementById("modals").innerHTML = comment;
-
-        var modal = new bootstrap.Modal(document.getElementById('modals'));
-        modal.show();
-    }
-
-}
-
-function getId(btn){
-    console.log("BUTTON ID =  " + btn.id);
 }
