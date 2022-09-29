304728d495f1430b38302f70ff2d1a1fe8d27d9d
Andrej Lazic <48242465+AndrejLazic@users.noreply.github.com>
2022-09-29 12:15:52 +0200

    Update script_class.js
    zavrsen preko klasa

--- script_class.js
+++ script_class.js
@@ -19,15 +19,25 @@ async function getPost(){
         j++;
         }
     }
-    
+
+    //console.log(merge)
     const post = new Post(merge);
+}
+
+async function getComments(btn){
+    console.log("BUTTON ID =  " + btn.id);
+
+    const response =  await fetch('https://jsonplaceholder.typicode.com/comments');
+    const comm = await response.json();
+
+    const comments = new Comments(comm, btn.id);
 }
 
 class Post{
     constructor(post){
         this.article = post;
-        
-        console.log(this.article);
+
+        //console.log(this.article);
 
         this.generatePosts();
     }
@@ -36,10 +46,10 @@ class Post{
         var card = "";
         for(let i=0; i<this.article.length; i++){
             card +=  `
-                <div class="container">             
+                <div class="container">
                 <div class="col mb-4">
                     <div class="card bg-black p-2 " style="--bs-bg-opacity: .23;">
-                        
+
                         <div class ="p-2 fs-5 text-left text-white">
                             <div>${this.article[i].title}
                                 <div class="text-end">
@@ -50,17 +60,17 @@ class Post{
                                 </div>
                             </div>
                         </div>
-                        
+
                         <div class ="card-body bg-black text-left text-white-50 rounded-3 m-2" style="--bs-bg-opacity: .37;">
                             <div class="price fs-7" class="price fs-8">${this.article[i].body}</div>
                         </div>
-                        
+
                         <div class="fs-6 text-white-50 mt-2 mb-2">
                             <div class="text-end">
                                 <abbr class="bg-black text-white-50 rounded-3 p-1" style="--bs-bg-opacity: .37;">
                                     <abbr title="attribute">pre 14 sati</abbr>
                                     <span id="username" class="fst-italic">${this.article[i].name}</span>
-                                    <button id='${this.article[i].id}' class="btn text-white-50 fst-italic"><b>Comments</b></button>
+                                    <button id='${this.article[i].id}' onclick="getComments(this)" class="btn text-white-50 fst-italic"><b>Comments</b></button>
                                 </abbr>
                             </div>
                         </div>
@@ -68,33 +78,64 @@ class Post{
                 </div>
                 </div>
                 `;
-            
         }
         document.getElementById("cards").innerHTML = card;
     }
-
-    getArictleId(e){
-        let button = e.target;
-        let buttonId = getAttribute('id');
-        let buttonValue = button.value;
-
-        console.log(buttonId);
-        console.log(buttonValue);
-    }
-}
-
-async function getComments(){
-    const response =  await fetch('https://jsonplaceholder.typicode.com/comments');
-    const comm = await response.json();
-
-    const comments = new Comments(comm)
 }
 
 class Comments{
-    constructor(comm){
+    constructor(comm, id){
+        this.comments = comm;
+        this.id = id;
 
+        this.getComments(this.id);
     }
-    generateComments(){
 
+    getComments(){
+        var data = this.comments;
+        var id = this.id;
+        let comment = [];
+        let modal = "";
+        for(let i=0; i < data.length; i++){
+            if(id == data[i].postId){
+                comment.push({
+                    id: data[i].id,
+                    postId: data[i].postId,
+                    title: data[i].name,
+                    body: data[i].body,
+                    email: data[i].email
+                });
+            }
+        }
+        console.log(comment);
+
+        for(let i=0; i<comment.length; i++){
+            modal +=
+                    `
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+
+                            <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">${comment[i].email}</h5>
+                            </div>
+
+                            <div class="modal-body">
+                            ${comment[i].title}
+                            </div>
+
+                            <div class="modal-body">
+                            ${comment[i].body}
+                            </div>
+
+                        </div>
+                    </div>
+                    `;
+        }
+        //console.log(modal);
+        document.getElementById("modals").innerHTML = modal;
+
+        var modals = new bootstrap.Modal(document.getElementById('modals'));
+        modals.show();
     }
+
 }
