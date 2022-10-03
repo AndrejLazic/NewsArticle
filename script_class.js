12fa722215b9bf72ba1609fc02300e8bfe5fd8f6
Andrej Lazic <ane.lazic@gmail.com>
2022-10-03 16:41:22 +0200

    MODAL - fade, dodat X

--- script_class.js
+++ script_class.js
@@ -117,6 +117,7 @@ class Comments{
 
                             <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">${comment[i].email}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
 
                             <div class="modal-body">
